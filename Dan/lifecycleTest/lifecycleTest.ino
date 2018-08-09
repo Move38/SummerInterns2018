193d358b94b3b5a90623182e20968cf3de966845
Jonathan Bobrow <jbobrow@gmail.com>
2018-08-08 21:34:38 -0400

    seperated logic from display... should be easier to set this up and then add in pretty animations

--- Dan/lifecycleTest/lifecycleTest.ino
+++ Dan/lifecycleTest/lifecycleTest.ino
@@ -1,154 +1,185 @@
-enum blinkStates {SLEEP, WAKE, PROD, GAMEA, GAMEB, MENU, PROGRAMA, PROGRAMB, LEARNA, LEARNB, READY, YAWN, KO};
+enum blinkStates {
+  SLEEP,
+  WAKE,
+  PROD,
+  GAMEA,
+  GAMEB,
+  MENU,
+  PROGRAMA,
+  PROGRAMB,
+  LEARNA,
+  LEARNB,
+  READY,
+  YAWN,
+  KO
+};
 byte blinkState;
+
+enum menuStates {
+  MENU_PROG,
+  MENU_SLEEP
+};
+byte menuState = MENU_PROG;
+
 bool menuToggle = true;
 byte currentGame = GAMEB;
 byte programType = PROGRAMA;
 bool programInitiator = false;
-Timer animTimer;
-int animFrame = 0;
+
+#define NUM_MENU_OPTIONS  2
+#define WAKE_DELAY      200
+#define WAKE_DURATION  1000
+#define SLEEP_DELAY     200
+#define LEARN_DURATION 6000
+#define PROG_DURATION  6000
+#define READY_DURATION  200
+
+#define MENU_TIMEOUT_DURATION    5000
+#define SLEEP_TIMEOUT_DURATION  20000
 
 Timer idleTimer;
 Timer menuIdleTimer;
+Timer progTimer;
 Timer learnTimer;
+Timer readyTimer;
+Timer wakeDelayTimer;
+Timer wakeDurationTimer;
+Timer sleepDelayTimer;
 
 void setup() {
   // put your setup code here, to run once:
   blinkState = WAKE;
-  idleTimer.set(60000);
+  wakeDelayTimer.set(WAKE_DELAY);
+  wakeDurationTimer.set(WAKE_DURATION);
+  idleTimer.set(SLEEP_TIMEOUT_DURATION);
 }
 
 void loop() {
   // first, the loop switch
   switch (blinkState) {
-    case SLEEP:
-      sleepLoop();
-      break;
-    case WAKE:
-    case PROD:
-      wakeLoop();
-      break;
-    case GAMEA:
-    case GAMEB:
-      gameLoop();
-      break;
-    case MENU:
-      menuLoop();
-      break;
-    case PROGRAMA:
-    case PROGRAMB:
-      programLoop();
-      break;
-    case LEARNA:
-    case LEARNB:
-      learnLoop();
-      break;
-    case READY:
-      readyLoop();
-      break;
-    case YAWN:
-    case KO:
-      yawnLoop();
-      break;
+    case SLEEP:     sleepLoop();    break;
+    case WAKE:      wakeLoop();     break;
+    case PROD:      wakeLoop();     break;
+    case GAMEA:     gameLoop();     break;
+    case GAMEB:     gameLoop();     break;
+    case MENU:      menuLoop();     break;
+    case PROGRAMA:  programLoop();  break;
+    case PROGRAMB:  programLoop();  break;
+    case LEARNA:    learnLoop();    break;
+    case LEARNB:    learnLoop();    break;
+    case READY:     readyLoop();    break;
+    case YAWN:      yawnLoop();     break;
+    case KO:        yawnLoop();     break;
   }
 
   //now some housekeeping, like making sure the sleep timer is reset after ANY interaction, and that you go to sleep if it expires
   if (buttonDown()) {
-    idleTimer.set(120000);
+    idleTimer.set(SLEEP_TIMEOUT_DURATION);
   }
 
+  // put idle blinks to sleep
   if (idleTimer.isExpired() && blinkState != SLEEP) {
-    setColor(OFF);
     blinkState = SLEEP;
-    buttonDoubleClicked();
-    buttonMenuPressed();
+  }
+
+  // display states
+  switch (blinkState) {
+    case SLEEP:     displaySleep();    break;
+    case WAKE:      displayWake();     break;
+    case PROD:      displayProd();     break;
+    case GAMEA:     displayGame();     break;
+    case GAMEB:     displayGame();     break;
+    case MENU:      displayMenu();     break;
+    case PROGRAMA:  displayProgram();  break;
+    case PROGRAMB:  displayProgram();  break;
+    case LEARNA:    displayLearn();    break;
+    case LEARNB:    displayLearn();    break;
+    case READY:     displayReady();    break;
+    case YAWN:      displayYawn();     break;
+    case KO:        displayKO();       break;
   }
 
   setValueSentOnAllFaces(blinkState);
 }
 
+/*
+    UPDATE LOOPS
+*/
+
 void sleepLoop() {
   if (buttonSingleClicked()) { //someone has hit the button. This is method 1 of waking up
     blinkState = WAKE;
+    wakeDelayTimer.set(WAKE_DELAY);
+    wakeDurationTimer.set(WAKE_DURATION);
   }
+  // button cleanup
+  buttonDoubleClicked();
 
   FOREACH_FACE(f) { //this looks for neighbors in WAKE state. Method 2 of waking up
     if (!isValueReceivedOnFaceExpired(f) && getLastValueReceivedOnFace(f) == PROD) {
       blinkState = WAKE;
-      idleTimer.set(60000);
+      wakeDelayTimer.set(WAKE_DELAY);
+      wakeDurationTimer.set(WAKE_DURATION);
+      idleTimer.set(SLEEP_TIMEOUT_DURATION);
     }
-  }
-
-  if (blinkState == WAKE) {
-    animFrame = 0;
   }
 }
 
 void wakeLoop() {
-  if (animTimer.isExpired()) { //do the wake animation.
-    animFrame++;
-    setColor(dim(YELLOW, animFrame * 17));
-    animTimer.set(100);
-  }
+  // button cleanup
+  buttonSingleClicked();
+  buttonDoubleClicked();
 
-  if (animFrame > 3) { //you are halfway through wakeup. Start waking neighbors
+  if (wakeDelayTimer.isExpired()) {
     blinkState = PROD;
+    wakeDelayTimer.set(NEVER);
   }
 
-  if (animFrame == 15) { //last frame of animation
+  if (wakeDurationTimer.isExpired()) { //last frame of animation
     blinkState = currentGame;
-    if (blinkState == GAMEA) {
-      setColor(ORANGE);
-    } else if (blinkState == GAMEB) {
-      setColor(RED);
-    }
+    wakeDurationTimer.set(NEVER);
   }
 }
 
+/*
+   Play game until one of the following is sensed
+   1. Menu Action (alone and pressed for > n seconds)
+   2. Neighbor is putting me to sleep (KO_STATE)
+   3. Neighbor is programming me (PROGRAM_STATE)
+*/
 void gameLoop() {
-  if (animTimer.isExpired()) {// do a basic game animation thing
-    if (blinkState == GAMEA) {//just a simple animation that turns on and off faces randomly
-      setColorOnFace(ORANGE, rand(5));
-      setColorOnFace(OFF, rand(5));
-      setColorOnFace(ORANGE, rand(5));
-      animTimer.set(rand(1) * 100 + 100);
-    } else if (blinkState == GAMEB) {//a simple animation that randomly fades a face
-      setColorOnFace(dim(RED, rand(255)), rand(5));
-      animTimer.set(150);
-    }
-  }
+  // button cleanup
+  buttonSingleClicked();
+  buttonDoubleClicked();
 
   //now we need to start looking out for menu press, but only when alone
   if (buttonMenuPressed()) {//you have to do this first to set the flag to false on every check
     if (isAlone()) {
       blinkState = MENU;
-      menuIdleTimer.set(10000);
-      setColorOnFace(dim(GREEN, 16), 0);
-      setColorOnFace(dim(GREEN, 16), 1);
-      setColorOnFace(dim(GREEN, 16), 2);
-      setColorOnFace(dim(BLUE, 16), 3);
-      setColorOnFace(dim(BLUE, 16), 4);
-      setColorOnFace(dim(BLUE, 16), 5);
-      animFrame = 0;
+      menuIdleTimer.set(MENU_TIMEOUT_DURATION);
     }
   }
 
   //we need to look out for a KO or programming signals from neighbors
   FOREACH_FACE(f) {
     if (!isValueReceivedOnFaceExpired(f)) {//you found a neighbor
-      if (getLastValueReceivedOnFace(f) == KO) { //KO command
+
+      byte neighbor = getLastValueReceivedOnFace(f);
+
+      if (neighbor == KO) {                // Neighbor is puttin us to sleep
+
         blinkState = YAWN;
-        menuIdleTimer.set(3000);//secondhand sleep timer
-        animFrame = 0;
-      } else if (getLastValueReceivedOnFace(f) == PROGRAMA) {
+        sleepDelayTimer.set(SLEEP_DELAY);
+      }
+      else if (neighbor == PROGRAMA && currentGame != GAMEA) {     // Neighbor is programming us game A
+
         blinkState = LEARNA;
-        learnTimer.set(6000);
-        setColor(GREEN);
-        animFrame = 0;
-      } else if (getLastValueReceivedOnFace(f) == PROGRAMB) {
+        learnTimer.set(LEARN_DURATION);
+      }
+      else if (neighbor == PROGRAMB && currentGame != GAMEB) {    // Neighbor is programming us game B
+
         blinkState = LEARNB;
-        learnTimer.set(6000);
-        setColor(GREEN);
-        animFrame = 0;
+        learnTimer.set(LEARN_DURATION);
       }
     }//end of found neighbor statement
   }//end of foreachface loop
@@ -156,196 +187,219 @@ void gameLoop() {
 
 void menuLoop() {
   if (buttonSingleClicked()) {
-    menuToggle = !menuToggle;//reverse the toggle
-    setColorOnFace(dim(GREEN, 16), 0);//reset all the dimness, let the next thing correct for choice
-    setColorOnFace(dim(GREEN, 16), 1);
-    setColorOnFace(dim(GREEN, 16), 2);
-    setColorOnFace(dim(BLUE, 16), 3);
-    setColorOnFace(dim(BLUE, 16), 4);
-    setColorOnFace(dim(BLUE, 16), 5);
-    menuIdleTimer.set(10000);
-  }
-
-  if (animTimer.isExpired()) {
-    animFrame++;
-    if (menuToggle) { //menu toggle true is in program choice
-      setColorOnFace(GREEN, 0);//reset all the greens
-      setColorOnFace(GREEN, 1);
-      setColorOnFace(GREEN, 2);
-      setColorOnFace(WHITE, animFrame % 3);//set one white
-      animTimer.set(300);
-    } else if (!menuToggle) { //menu toggle false is in sleep choice
-      setColorOnFace(dim(BLUE, abs(animFrame - 15) * 17), 3);
-      setColorOnFace(dim(BLUE, abs(animFrame - 15) * 17), 4);
-      setColorOnFace(dim(BLUE, abs(animFrame - 15) * 17), 5);
-      animTimer.set(100);
-    }
-
-    if (animFrame == 30) {
-      animFrame = 0;
-    }
+    menuState = ( menuState + 1 ) % NUM_MENU_OPTIONS;
+    menuIdleTimer.set(MENU_TIMEOUT_DURATION);
   }
 
   if (buttonDoubleClicked()) { //type selected, move to appropriate mode
-    if (menuToggle) {//head to the correct programming mode
+
+    menuIdleTimer.set(MENU_TIMEOUT_DURATION);
+
+    if (menuState == MENU_PROG) {//head to the correct programming mode
       blinkState = programType;
       programInitiator = true;
-    } else if (!menuToggle) {
+    } else if (menuState == MENU_SLEEP) {
       blinkState = YAWN;
-      menuIdleTimer.set(10000);
-      animFrame = 0;
     }
   }
 
   if (menuIdleTimer.isExpired()) {
     blinkState = currentGame;
-    if (blinkState == GAMEA) {
-      setColor(ORANGE);
-    } else if (blinkState == GAMEB) {
-      setColor(RED);
-    }
   }
 }
 
 void programLoop() {
-  if (animTimer.isExpired()) {//just the animation
-    animFrame++;
-    setColor(GREEN);
-    setColorOnFace(WHITE, animFrame % 3);
-    setColorOnFace(WHITE, (animFrame % 3) + 3);
-    animTimer.set(100);
-  }
 
   if (buttonDoubleClicked()) {//return to menu if double clicked
     blinkState = MENU;
-    setColorOnFace(dim(GREEN, 16), 0);//reset all the dimness, let the next thing correct for choice
-    setColorOnFace(dim(GREEN, 16), 1);
-    setColorOnFace(dim(GREEN, 16), 2);
-    setColorOnFace(dim(BLUE, 16), 3);
-    setColorOnFace(dim(BLUE, 16), 4);
-    setColorOnFace(dim(BLUE, 16), 5);
-    animFrame = 0;
-    menuIdleTimer.set(10000);
+    menuIdleTimer.set(MENU_TIMEOUT_DURATION);
     programInitiator = false;
   }
+  // button cleanup
+  buttonSingleClicked();
 
   //here we need to determine if it's safe to move to READY state
   //to qualify, all neighbors must be in PROGRAMX or READY. No learners left
-  if (!isAlone()) {//we have some neighbors, lets look at them
-    bool readyCheck = false;
-    FOREACH_FACE(f) {
-      if (!isValueReceivedOnFaceExpired(f)) {//only do this check on spots WITH neighbors
-        if (getLastValueReceivedOnFace(f) == PROGRAMA || getLastValueReceivedOnFace(f) == PROGRAMB || getLastValueReceivedOnFace(f) == READY) {
-          //this face is good
-          readyCheck = true;
-        } else {
-          //in some other state
-          readyCheck = false;
-        }
+  bool readyCheck = true;
+
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {//only do this check on spots WITH neighbors
+
+      byte neighbor = getLastValueReceivedOnFace(f);
+
+      if (neighbor == LEARNA || neighbor == LEARNB || neighbor == READY) {
+        // don't do anything
+      }
+      else {
+        // if a neighbor is still
+        readyCheck = false;
       }
     }
-    //so the face loop is over. If the readyCheck is true, we're golden
-    if (readyCheck) {
-      blinkState = READY;
-      setColor(dim(GREEN, 16));//just set it to a dim green
-      if (programInitiator) { //so for the one who begins things, THIS is where they change currentGame
-        currentGame = programType;
-      }
+  }
+
+  // if we are alone, we are not ready to be ready :)
+  if (isAlone()) {
+    readyCheck = false;
+  }
+
+  //so the face loop is over. If the readyCheck is true, we're golden
+  if (readyCheck) { // && progTimer.isExpired()) {  // only become ready after our programming time is done and our neighbors have received the game
+    blinkState = READY;
+    if (programInitiator) { //so for the one who begins things, THIS is where they change currentGame
+      currentGame = getMyKnownGame();
+//      blinkState = currentGame; // update to the game we know and shared when our neighbors have learned and are ready
     }
   }
 
   if (menuIdleTimer.isExpired()) {
     blinkState = currentGame;
-    if (blinkState == GAMEA) {
-      setColor(ORANGE);
-    } else if (blinkState == GAMEB) {
-      setColor(RED);
-    }
     programInitiator = false;
   }
 }
 
-void learnLoop() {
-  if (animTimer.isExpired()) {//a simple green fading animation
-    animFrame ++;
-    animTimer.set(100);
-    setColor(dim(GREEN, abs(animFrame - 15) * 17));
+byte getMyKnownGame() {
+
+  byte game;
+
+  if (programType == PROGRAMA) {
+    game = GAMEA;
+  }
+  else if (programType == PROGRAMB) {
+    game = GAMEB;
   }
 
-  if (animFrame == 30) {
-    animFrame = 0;
-  }
+  return game;
+}
+
+void learnLoop() {
+  // button cleanup
+  buttonSingleClicked();
+  buttonDoubleClicked();
+
+  // we are learning here, no going to sleep now
+  idleTimer.set(SLEEP_TIMEOUT_DURATION);
 
   if (learnTimer.isExpired()) {//this means the game has been "learned" and we can move to PROGRAM state
     if (blinkState == LEARNA) {
       currentGame = GAMEA;
-      blinkState = PROGRAMA;
-    } else if (blinkState == LEARNB) {
+      blinkState = PROGRAMA;  // now that we've learned, share the knowledge
+      progTimer.set(PROG_DURATION);
+    }
+    else if (blinkState == LEARNB) {
       currentGame = GAMEB;
-      blinkState = PROGRAMB;
+      blinkState = PROGRAMB;  // now that we've learned, share the knowledge
+      progTimer.set(PROG_DURATION);
     }
   }
 }
 
 void readyLoop() {
+  // button cleanup
+  buttonSingleClicked();
+  buttonDoubleClicked();
+
   //so now we look at all neighbors and determine if all are in READY or GAME so we can transition to game state
-  bool gameCheck = false;
+  bool isSuroundedByCurrentGameOrReady = true;
+
   FOREACH_FACE(f) {
+
     if (!isValueReceivedOnFaceExpired(f)) {//only check occupied faces
-      if (getLastValueReceivedOnFace(f) == READY || getLastValueReceivedOnFace(f) == currentGame) {
+
+      byte neighbor = getLastValueReceivedOnFace(f);
+
+      if (neighbor != READY && neighbor != currentGame) {
         //this face is good
-        gameCheck = true;
-      } else {
-        //this face is no good
-        gameCheck = false;
+        isSuroundedByCurrentGameOrReady = false;
       }
     }
   }
 
-  if (gameCheck) { //survived the loop while still true
+  if (isSuroundedByCurrentGameOrReady) { //survived the loop while still true
     blinkState = currentGame;
-    if (currentGame == GAMEA) {
-      setColor(ORANGE);
-    } else if (currentGame = GAMEB) {
-      setColor(RED);
-    }
-    animFrame = 0;
-    idleTimer.set(1000);
+    // TODO: Why is the idle timer getting set here? Isn't that for putting a Blink to fake sleep?
+    // idleTimer.set(1000);
   }
 }
 
 void yawnLoop() {
-  if (animTimer.isExpired()) {
-    animFrame++;
-    setColor(dim(BLUE, abs(animFrame - 15) * 17));
-    animTimer.set(100);
-  }
+  // button cleanup
+  buttonSingleClicked();
+  buttonDoubleClicked();
 
-  if (animFrame == 3) {
+  if (sleepDelayTimer.isExpired()) {
     blinkState = KO;
-  }
-
-  if (animFrame == 30) {
-    animFrame = 0;
   }
 
   if (buttonDoubleClicked()) {//return to menu if double clicked
     blinkState = MENU;
-    setColorOnFace(dim(GREEN, 16), 0);//reset all the dimness, let the next thing correct for choice
-    setColorOnFace(dim(GREEN, 16), 1);
-    setColorOnFace(dim(GREEN, 16), 2);
-    setColorOnFace(dim(BLUE, 16), 3);
-    setColorOnFace(dim(BLUE, 16), 4);
-    setColorOnFace(dim(BLUE, 16), 5);
-    animFrame = 0;
-    menuIdleTimer.set(10000);
+    menuIdleTimer.set(MENU_TIMEOUT_DURATION);
   }
 
   if (menuIdleTimer.isExpired()) {
     blinkState = SLEEP;
-    setColor(OFF);
-    buttonDoubleClicked();
-    buttonMenuPressed();
   }
+}
+
+/*
+    DISPLAY LOOPS
+*/
+void displaySleep() {
+  setColor(dim(BLUE, 64));
+}
+
+void displayWake() {
+  setColor(YELLOW);
+}
+
+void displayProd() {
+  setColor(YELLOW);
+  setColorOnFace(WHITE, 1);
+}
+
+void displayGame() {
+  if (blinkState == GAMEA) {
+    setColor(ORANGE);
+  }
+  else if (blinkState == GAMEB) {
+    setColor(RED);
+  }
+}
+
+void displayMenu() {
+  switch (menuState) {
+    case MENU_PROG: setColor(OFF); setColorOnFace(GREEN, 4); break;
+    case MENU_SLEEP: setColor(OFF); setColorOnFace(BLUE, 1); break;
+  }
+}
+
+void displayProgram() {
+  setColor(GREEN);
+}
+
+void displayLearn() {
+  setColor(dim(GREEN, 127));
+}
+
+void displayReady() {
+  setColor(GREEN);
+  setColorOnFace(WHITE, 1);
+}
+
+void displayYawn() {
+  setColor(BLUE);
+}
+
+void displayKO() {
+  setColor(BLUE);
+  setColorOnFace(WHITE, 1);
+  setColorOnFace(WHITE, 3);
+  setColorOnFace(WHITE, 5);
+}
+
+/*########################################
+       ONLY FOR MASTER API BRANCH
+  ########################################*/
+bool buttonMenuPressed() {
+  return buttonLongPressed();
 }
 
